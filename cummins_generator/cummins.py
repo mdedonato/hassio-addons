93b0e838f5f85e45bdd5198e1c1d7f361e9f1454
Matt DeDonato <matt.dedonato@gmail.com>
2021-10-04 23:16:17 -0400

    Update cummins.py

--- cummins_generator/cummins.py
+++ cummins_generator/cummins.py
@@ -420,15 +420,6 @@ def service_shutdown(signum, frame):
     _RUNNING = False
     raise ServiceExit
 
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        logger.info("Connected to MQTT Broker!")
-    else:
-        logger.error("Failed to connect, return code %d\n", rc)
-
-def on_log(client, userdata, level, buf):
-    logger.debug("log: ",buf)
-
 
 def main():
     try:
@@ -461,9 +452,8 @@ def main():
         mqtt_client = mqtt.Client("cummins")
         logger.debug('Starting Time Thread')
         time_thread = multiprocessing.Process(target=time_sync, args=(cummins,int(config['CUMMINS']['TimeSyncMin'])*60))
-        mqtt_client.on_connect = on_connect
+        mqtt_client.enable_logger(logger)
         mqtt_client.username_pw_set("mqttuser", "mqttuser")
-        mqtt_client.on_log=on_log
         mqtt_client.connect(config['MQTT']['Host'])
         cummins.subscribe_mqtt(mqtt_client)
         mqtt_client.loop_start()
